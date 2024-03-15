6518e3dc6c6805c816b748cad7f558c7bf5a505d
Battatt <151041544+Battatt@users.noreply.github.com>
2024-03-15 18:27:00 +0300

    Added base.html

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-import flask
+from flask import Flask, render_template
 
 
-print(type(flask))
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title="main"):
+    return render_template("index.html", title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='127.0.0.1')
